c30ed11cfea91b6b213d824d7f64e9d65e3fea0c
mini <191342998+mini101591@users.noreply.github.com>
2025-06-08 23:24:36 +1000

    Working on the Styling and custom components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,36 @@
-import { View, Text, Image, ImageBackground,ScrollView, Button,Pressable,Modal } from "react-native";
-import logoImg = require("../../assets/images/adaptive-icon.png");
-import { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Greet from "@/components/Greet";
+
+
 
 export default function App(){
-  const[isModalVisible,setIsModalVisible]= useState(false);
-  return <View style={{flex:1,backgroundColor:"plum", padding:60}}>
-    <ScrollView>
-      <Button 
-        title="Click"
-        onPress={()=>setIsModalVisible(true)}
-        color="midnightblue"
-      />
-      <Modal 
-      visible={isModalVisible}
-      onRequestClose={()=>setIsModalVisible(false)}
-      animationType="slide"
-      presentationStyle="pageSheet"
-      >
-        <View style={{flex:1,backgroundColor:"lightblue", padding:60}}>
-          <Text>Welcome To My Page...</Text>
-          <Button title="Close" onPress={()=>setIsModalVisible(false)}/>
-        </View>
-      </Modal>
-      <Button title="try me" disabled/>
-      <Pressable onPress={()=>{console.log("Image Pressed");}}><Image source={logoImg} style={{width:200, height:300}}/></Pressable>
-      <Text><Text style={{color:"green"}}>Hello</Text>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-      </Text>
-    {/*<Image source={{uri:"https://picsum.photos/200"}} style={{height:200,width:200}}/>
-    <ImageBackground source={logoImg} style={{flex:1}}>
-      <Text>Text Image</Text>
-    </ImageBackground>*/}
-    </ScrollView>
+  return <View style={styles.container}>
+    <Text style={styles.heading}>Stylesheet API</Text>
+    <Greet name="John" />
+    <Greet name="Kale" />
+    <View style={[styles.box,styles.lightblueBg]}>
+      <Text>LIGHTBLUEBOX</Text>
+    </View>
+     <View style={[styles.box,styles.lightgreenBg]}>
+      <Text>LIGHTGREENBOX</Text>
+    </View>
   </View>
 }
+
+export const styles = StyleSheet.create({
+  container:{flex:1,backgroundColor:"plum", padding:60},
+  heading:{fontSize:20},
+  subheading:{fontSize:10,fontStyle:"any",color:"red"},
+  box:{
+    height:100,
+    width:100,
+    padding:10
+  },
+  lightblueBg:{
+    backgroundColor:"lightblue"
+  },
+  lightgreenBg:{
+    backgroundColor:"lightgreen"
+  }
+});
+
